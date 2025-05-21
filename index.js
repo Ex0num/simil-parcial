b02257fca15959d5b65bd26fe628fc1af94787e1
ex0num <gabriel.lopez.gasal@gmail.com>
2025-05-20 22:17:56 -0300

    Lo dejamos funcionando

--- index.js
+++ index.js
@@ -1,25 +1,152 @@
 //--- Funcion que obtiene el carrito del LocalStorage, lo parsea a un array y lo retorna ---//
 function obtenerCarrito() 
 {
-    return;
+    let carritoObtenido = localStorage.getItem("carrito");
+    let carritoParseado = JSON.parse(carritoObtenido);
+    return carritoParseado;
 }
 
 //--- Funcion que guarda el carrito recibido al LocalStorage, previamente transformado a string ---//
 function guardarCarrito(carrito) 
 {
-
+   let carritoTransformadoAString = JSON.stringify(carrito);
+   localStorage.setItem("carrito", carritoTransformadoAString);
 }
 
 function sumarAlCarrito(e) 
 {
+    console.log("Sumar a carrito llamado");
+
     //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
     let elementoClickeado = e.target;
+
+    let itemContenedor = elementoClickeado.parentNode;
+    
+    let arrayHijosItemContenedor = itemContenedor.childNodes;
+
+    let elementoNombre = itemContenedor.querySelector(".nombre-producto");
+    let elementoPrecio = itemContenedor.querySelector(".precio-producto");
+    let elementoDescripcion = itemContenedor.querySelector(".descripcion-producto");
+
+    let nombreDelProducto = elementoNombre.textContent;
+    let precioDelProducto = elementoPrecio.textContent;
+    let textoDescripcion = elementoDescripcion.textContent;
+
+    let productoPreExistente = false;
+
+    let carritoParseado = obtenerCarrito();
+
+
+    // Me fijo si habia algo cargado en el LocalStorage como "carrito"
+    if (carritoParseado)
+    {
+        // Buscar si el elemento existia (para sumarle 1)
+        for (let element of carritoParseado) 
+        {
+            if (element.nombre == nombreDelProducto) //Ya existe el producto clickeado
+            {
+                console.log("Elemento ya preexistia");
+                element.cantidad = element.cantidad + 1;
+                productoPreExistente = true;
+                break;
+            }
+        }
+    }
+    else
+    {
+        //--- No existe el carrito ---//
+        carritoParseado = [];
+    }
+
+    if (productoPreExistente == false)
+    {
+        //--- Ya existia el carrito ---//
+        let nuevoProducto = 
+        {
+            "nombre": nombreDelProducto,
+            "precio": precioDelProducto,
+            "descripcion": textoDescripcion,
+            "cantidad": 1
+        }
+
+        carritoParseado.push(nuevoProducto);
+    }
+
+    guardarCarrito(carritoParseado);
+    alert(`Un/una ${nombreDelProducto} fue agregado al carrito`);
 }
 
 function restarDelCarrito(e) 
 {
     //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
     let elementoClickeado = e.target;
+
+    console.log("Restar a carrito llamado");
+
+    let itemContenedor = elementoClickeado.parentNode;
+    
+    let arrayHijosItemContenedor = itemContenedor.childNodes;
+
+    let elementoNombre = itemContenedor.querySelector(".nombre-producto");
+    let elementoPrecio = itemContenedor.querySelector(".precio-producto");
+    let elementoDescripcion = itemContenedor.querySelector(".descripcion-producto");
+
+    let nombreDelProducto = elementoNombre.textContent;
+    let precioDelProducto = elementoPrecio.textContent;
+    let textoDescripcion = elementoDescripcion.textContent;
+
+    let productoPreExistente = false;
+
+    let carritoParseado = obtenerCarrito();
+
+
+    // Me fijo si habia algo cargado en el LocalStorage como "carrito"
+    if (carritoParseado)
+    {
+        // Buscar si el elemento existia (para sumarle 1)
+        for (let element of carritoParseado) 
+        {
+            if (element.nombre == nombreDelProducto) //Ya existe el producto clickeado
+            {
+                console.log("Elemento ya preexistia");
+                element.cantidad = element.cantidad + -1;
+                productoPreExistente = true;
+                break;
+            }
+        }
+    }
+    else
+    {
+        //--- No existe el carrito ---//
+        carritoParseado = [];
+    }
+
+    if (carritoParseado.length == 0)
+    {
+        alert("No hay ningun producto en el carrito");
+    }
+
+    if (productoPreExistente == false)
+    {
+        //--- Ya existia el carrito ---//
+        alert("No hay ningun producto cargado con ese nombre");
+    }
+    else
+    {
+        carritoParseado = carritoParseado.filter( (element) => 
+        {
+            if (element.cantidad == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        });
+    }
+
+    guardarCarrito(carritoParseado);
 }
 
 //--- [EVENTOS] Asociacion del evento "click" a los botones "+" y "-" con la funcion manejadora del evento ---//
--- pages/carrito/carrito.js
+++ pages/carrito/carrito.js
@@ -1,16 +1,76 @@
+//--- Funcion que obtiene el carrito del LocalStorage, lo parsea a un array y lo retorna ---//
 function obtenerCarrito() 
 {
+    let carritoObtenido = localStorage.getItem("carrito");
+    let carritoParseado = JSON.parse(carritoObtenido);
     return carritoParseado;
 }
 
 function cargarProductosCarrito() 
 {
-  
+    let carritoParseado = obtenerCarrito();
+
+    let referenciaTabla = document.getElementById("tabla-carrito");
+
+    let precioProductoString = "";
+    let precioProductoNumber = 0;
+    let precioFinalProducto = 0;
+    
+    let acumuladorPrecioTotal = 0;
+
+    if (carritoParseado)
+    {
+        for (const element of carritoParseado) 
+        {
+            //--- Creo la nueva fila ---//
+            let nuevaFila = document.createElement("tr");
+
+            //--- Creo las celdas que necesito y les meto el dato ---//
+            let celdaNombre = document.createElement("td");
+            celdaNombre.innerText = element.nombre;
+
+            let celdaCantidad = document.createElement("td");
+            celdaCantidad.innerText = element.cantidad;
+
+            let celdaPrecio = document.createElement("td");
+            celdaPrecio.innerText = element.precio;
+
+            //--- Agrego como hijas las celdas a la fila ---//
+            nuevaFila.appendChild(celdaNombre);
+            nuevaFila.appendChild(celdaCantidad);
+            nuevaFila.appendChild(celdaPrecio);
+            
+            //--- Agrego la fila final a la tabla ---//
+            referenciaTabla.appendChild(nuevaFila);
+
+            precioProductoString = element.precio;
+            precioProductoString = precioProductoString.replace("$","");
+            precioProductoNumber = parseFloat(precioProductoString);
+            precioFinalProducto = precioProductoNumber * element.cantidad;
+
+            acumuladorPrecioTotal = acumuladorPrecioTotal + precioFinalProducto;
+        }
+
+        let textoValorFinal = document.getElementById("valor-final");
+        textoValorFinal.innerText = `El valor final a pagar es de: ${acumuladorPrecioTotal}`;
+    }
+    else
+    {
+        alert("No hubo elementos en el carrito");
+    }
+
 }
 
 function limpiarCarrito() 
 {
+    localStorage.removeItem("carrito");
+    alert("Carrito limpiado correctamente");
     
+    let referenciaTabla = document.getElementById("tabla-carrito");
+    referenciaTabla.style.display = "none";
+
+    let textoValorFinal = document.getElementById("valor-final");
+    textoValorFinal.innerText = `El valor final a pagar es de: $0`;
 }
 
 // Asociar evento al botón cuando la página carga
